4cbbbf9986b030f7bf76ff87ee7c1d2d493d5133
FrolovNM <nikitka_frolov_2014@inbox.ru>
2026-02-16 16:39:42 +0300

    формат

--- src/main/java/com/frolovsnails/util/DateTimeUtils.java
+++ src/main/java/com/frolovsnails/util/DateTimeUtils.java
@@ -18,7 +18,7 @@ public class DateTimeUtils {
 
     // Для API ответов (без миллисекунд)
     private static final DateTimeFormatter ISO_FORMATTER =
-            DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+            DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm"); 
 
     /**
      * Получить текущее время в Москве
